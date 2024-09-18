8239ee199878be359d32db9da888ae70d173dfb0
Edvard <e.smoliakovas@gmail.com>
2024-09-18 16:51:14 +0300

    More project

--- ReadingSpeed/obj/Debug/net8.0/ReadingSpeed.AssemblyInfo.cs
+++ ReadingSpeed/obj/Debug/net8.0/ReadingSpeed.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReadingSpeed")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1561b9c39e1eb99e682fb2889aaf9223df3bae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+870a1ba40994ee6fde9f2c96b8dab8aa998a3812")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReadingSpeed")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReadingSpeed")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
